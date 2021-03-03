e45d46f541fe1de043c0121644a2cef0d3694c97
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 19:33:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,4 @@
-def collect(collection)
+def my_collect(collection)
 #were iterating over a collection with a while loop, executing a block of code,return collect
 i = 0
   new_collection = []
@@ -10,13 +10,3 @@ new_collection  << yield(collection[i])
 
 end
 
-
-def my_collect(collection)
-  i = 0
-  new_collection = []
-  while i < collection.length
-    new_collection << yield(collection[i])
-    i += 1
-  end
-  new_collection
-end
