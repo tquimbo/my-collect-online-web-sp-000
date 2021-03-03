60ad5586c7f7083550d37fdf009e92c2db03811e
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 19:34:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+#were iterating over a collection with a while loop, executing a block of code,return collect
+i = 0
+  new_collection = []
+while i < collection.length
+new_collection  << yield(collection[i])
+    i += 1
+  end
+ new_collection
 
-
+end
