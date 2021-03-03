df6782c41d8dd4ea7cba81e76ac5c430875731a3
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 19:24:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def collect(collection)
-#were iterating over a collection, executing a block of code,return collect
-
+#were iterating over a collection with a while loop, executing a block of code,return collect
+counter = 0
   collection = []
-  my_collect(collection) do |lang|
+while i < collection.length
+  my_collect(collection) do |x|
     lang.upcase
+    counter +=x
   end
 return collection
 
