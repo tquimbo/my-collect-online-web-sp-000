b779f84e5bf5d1406305ca36070fedc686fa4c2a
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 19:32:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,3 +9,14 @@ new_collection  << yield(collection[i])
  new_collection
 
 end
+
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
