3b71bb7a20c2f6460a0854a70af5f9b22e2b393b
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 19:19:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def collect(collection)
 #were iterating over a collection, executing a block of code,return collect
   i = 0
-  collection = []
+  collection = ["Tim", "Timmy", "Timbo"]
   while i < array.length
     collection << yield(array[i])
     i += 1
